bb6d4dec0951893f3a76051c1773a88cd68c41ae
fduxiao <9294979+fduxiao@users.noreply.github.com>
2024-05-15 02:48:06 -0400

    add contents

--- QuickStart.lean
+++ QuickStart.lean
@@ -2,3 +2,10 @@
 -- Import modules here that should be built as part of the library.
 import «QuickStart».Basic
 import «QuickStart».IndPrinciples
+
+
+/-!
+# Contents
+- `QuickStart.Basic`: how to design new type and functions, write some simple proofs.
+- `QuickStart.IndPrinciples`: how the tactic works and how to define ∃, =
+-/
